f70769267eceeb162281652bf6fe13150b6b200e
Fabio Penna <ffmpenna@gmail.com>
2023-01-28 14:16:32 -0300

    feat: configuração do JWT

---
+++ src/utils/JWT.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || '&EsE&HRaUHa&2Sr3ti1aM&SNECpNe';
+
+const jwtCfg = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const generateToken = (payload) => jwt.sign(payload, JWT_SECRET, jwtCfg);
+
+const authenticateToken = async (token) => {
+  if (!token) {
+    return { type: 'MISSING_AUTH_TOKEN', message: 'Missing auth token' };
+  }
+
+  try {
+    const decodedData = await jwt.verify(token, JWT_SECRET);
+    return decodedData;
+  } catch (err) {
+    return { type: 'JWT_MALFORMED', message: 'JWT malformed' };
+  }
+};
+
+module.exports = {
+  generateToken,
+  authenticateToken,
+};
